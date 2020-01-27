0c1c37cb1d7aa1ee4a9b4acacce90fe189fc3199
Mirko Kopsa <mirko.kopsa@gmail.com>
2020-01-27 21:21:14 +0200

    Lab 2

--- app/src/main/java/com/mobicomp/labs/MainActivity.kt
+++ app/src/main/java/com/mobicomp/labs/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.mobicomp.labs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test_button.setOnClickListener{
+            toast("Oh hello")
+        }
+
+        fab.setOnClickListener{
+            toast("Floating action button!")
+        }
+
+        openTimeActivity.setOnClickListener{
+            startActivity(Intent(applicationContext, TimeActivity::class.java))
+        }
+
+        openMapActivity.setOnClickListener{
+            startActivity(Intent(applicationContext, MapActivity::class.java))
+        }
     }
 }
