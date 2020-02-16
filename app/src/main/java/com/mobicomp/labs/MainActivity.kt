ad4ad7dcfafd1846ba1a0eeccf1be73ca23db650
Mirko Kopsa <mirko.kopsa@gmail.com>
2020-02-16 22:18:36 +0200

    Lab 4

--- app/src/main/java/com/mobicomp/labs/MainActivity.kt
+++ app/src/main/java/com/mobicomp/labs/MainActivity.kt
@@ -3,8 +3,11 @@ package com.mobicomp.labs
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,14 +27,42 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(applicationContext, TimeActivity::class.java))
         }
 
-        openMapActivity.setOnClickListener{
+        openMapActivity.setOnClickListener {
             startActivity(Intent(applicationContext, MapActivity::class.java))
         }
 
-        val data = arrayOf("Oulu", "Helsinki", "Tampere")
+    }
 
-        val reminderAdapter = ReminderAdapter(applicationContext, data)
-        list.adapter = reminderAdapter
+    override fun onResume() {
+
+        super.onResume()
+        refreshList()
+
+    }
+
+    private fun refreshList () {
+
+        doAsync {
+
+            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "reminders").build()
+            val reminders = db.reminderDao().getReminders()
+
+            db.close()
+
+            uiThread {
+
+                if (reminders.isNotEmpty()) {
+
+                    val adapter = ReminderAdapter(applicationContext, reminders)
+                    list.adapter = adapter
+
+                } else {
+                    toast("No reminders yet")
+                }
+
+            }
+
+        }
 
     }
 }
--- app/src/main/java/com/mobicomp/labs/TimeActivity.kt
+++ app/src/main/java/com/mobicomp/labs/TimeActivity.kt
@@ -1,12 +1,78 @@
 package com.mobicomp.labs
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import androidx.room.Room
+import kotlinx.android.synthetic.main.activity_time.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.toast
+import java.util.*
 
 class TimeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_time)
+
+        timeCreate.setOnClickListener {
+
+            val calendar = GregorianCalendar(
+                datePicker.year,
+                datePicker.month,
+                datePicker.dayOfMonth,
+                timePicker.currentHour,
+                timePicker.currentMinute
+            )
+
+            if ((editTextMessage.text.toString() != "") &&
+                (calendar.timeInMillis > System.currentTimeMillis())) {
+
+                val reminder = Reminder(
+                    uId = null,
+                    time = calendar.timeInMillis,
+                    location = null,
+                    message = editTextMessage.text.toString()
+                )
+
+                doAsync {
+
+                    val db = Room.databaseBuilder(
+                        applicationContext,
+                        AppDatabase::class.java,
+                        "reminders").build()
+                    db.reminderDao().insert(reminder)
+                    db.close()
+
+                    setAlarm(reminder.time!!, reminder.message)
+
+                    finish()
+                }
+
+            } else {
+                toast("Wrong data")
+            }
+
+        }
+
     }
+
+    private fun setAlarm(time: Long, message: String) {
+
+        val intent = Intent(this, ReminderReceiver::class.java)
+        intent.putExtra("message", message)
+
+        val pendingIntent = PendingIntent.getBroadcast(this, 1, intent, PendingIntent.FLAG_ONE_SHOT)
+
+        val manager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        manager.setExact(AlarmManager.RTC, time, pendingIntent)
+
+        runOnUiThread(toast("Reminder created"))
+
+    }
+
 }
